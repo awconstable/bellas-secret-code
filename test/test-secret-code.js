a28d5ca69988eb5a038513a47c8d7368a9e24bbe
Austen Constable <awconstable@gmail.com>
2019-11-13 13:24:54 +0000

    Handle uppercase and characters we can't encode

--- test/test-secret-code.js
+++ test/test-secret-code.js
@@ -4,13 +4,23 @@ var assert = require('assert');
 
 describe('secretCode', function(){
   describe('#encodeLetter("a")', function(){
-    it('should return the smiley emoji codepoint code', function(){
+    it('should return the smiley emoji codepoint code for a', function(){
       assert.equal(secretCode.encodeLetter("a"), "😃");
     });
   });
-  describe('#encodeWord("bella")', function(){
+  describe('#encodeLetter("A")', function(){
+    it('should return the smiley emoji codepoint code for a', function(){
+      assert.equal(secretCode.encodeLetter("A"), "😃");
+    });
+  });
+  describe('#encodeLetter("!")', function(){
+    it('should return the unencodable character back', function(){
+      assert.equal(secretCode.encodeLetter("!"), "!");
+    });
+  });
+  describe('#encodeWord("Bella")', function(){
     it('should return the smiley emoji codepoint codes for an entire word', function(){
-      assert.equal(secretCode.encodeWord("bella"), "😋😂🙄🙄😃");
+      assert.equal(secretCode.encodeWord("Bella!!!!"), "😋😂🙄🙄😃!!!!");
     });
   });
 })
