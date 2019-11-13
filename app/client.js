5c8df8e956faced4c2d552d0227390d4bb271793
Austen Constable <awconstable@gmail.com>
2019-11-13 15:25:48 +0000

    Added ability to dynamically encode and decode

--- app/client.js
+++ app/client.js
@@ -2,5 +2,13 @@ var $ = require("jquery");
 var secretCode = require('./../lib/secret-code');
 
 $(document).ready(function(){
-    $("#container").text("Hello, my name is " + secretCode.encodeWord("bella"));
+    var encodeInput = $("#encode-input");
+    encodeInput.on('input propertychange paste',function() {
+        $("#encode-output").val(secretCode.encodeWord(encodeInput.val()));
+    });
+
+    var decodeInput = $("#decode-input");
+    decodeInput.on('input propertychange paste',function() {
+        $("#decode-output").val(secretCode.decodeWord(decodeInput.val()));
+    });
 });
